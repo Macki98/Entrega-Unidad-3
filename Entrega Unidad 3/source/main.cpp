fd272814749ed3e805247fb89fab037cc31e9ff7
Leonel Mc Cormack <167136818+Macki98@users.noreply.github.com>
2025-11-05 15:37:04 -0300

    Entrega Unidad 4 (en proceso)

--- Entrega Unidad 3/source/main.cpp
+++ Entrega Unidad 3/source/main.cpp
@@ -17,122 +17,122 @@ int main(void)
     // Aplicamos un filtro bilineal a la textura
     SetTextureFilter(imagen, TEXTURE_FILTER_BILINEAR);
 
-    // Posicion inicial de la nave 1 y nave 2
+    // Cargamos la textura del cursor, aplicamos filtro y ocultamos el cursor del sistema
+    Texture2D cursor = LoadTexture("Assets/crosshair.png");
+    SetTextureFilter(cursor, TEXTURE_FILTER_BILINEAR);
+    HideCursor();
+
+    // Aplicamos el origen del cursor para que responda a la posicion actual del mouse
+    Rectangle cursorRectangle = { 0,0, cursor.width, cursor.height};
+    Vector2 cursorCenter = { cursor.width / 2, cursor.height / 2 };
+
+
+    // Color del fondo
+    Color fondo = RAYWHITE;
+
+    // Color del Boton
+    Color colorBoton = BLACK;
+
+    // Radio del Boton
+    float r = 50;
+
+    // Boton para cambiar de color el fondo
+    Vector2 Boton = { 970, 50 };
+
+
+    // Posicion inicial de la nave 1
     Vector2 posImagen1 = {150, (768 / 2)};
-    Vector2 posImagen2 = { 800, 500 };
 
     // velocidad que se le asignara a la nave
     Vector2 velocidad = { 5.0f, 5.0f };
 
-    // Color de la nave 1 y nave 2
+    // Color de la nave 1
     Color colImagen1 = {255,161,0,255};
-    Color colImagen2 = {0,158,47,255}; 
 
-
-    // Rotacion de la nave 1 y nave 2
+    // Rotacion de la nave 1 
     float rotImagen1 = 0.0f;
-    float rotImagen2 = 180.0f;
-
-    // Escalado de la nave 1 y nave 2
+  
+    // Escalado de la nave 1
     float escImagen1 = 0.5f;
-    float escImagen2 = 0.3f;
 
 
     // Configuracion framerate
     SetTargetFPS(60);
 
-    // Contador de veces presionada la barra espaciadora para activar el SWAP visual
+    // Contador de veces presionada la tecla M para mostrar/ocultar msj de sistema
     int contador = -1;
     
     // Bucle principal del juego (se repite hasta que se cierre la ventana)
     while (!WindowShouldClose())
     {
+        // Buffer de teclado que detecta si se presiono la tecla ESC para cerrar la ventana
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            CloseWindow();
+        }
 
-        // Aplicamos movimiento sobre la Nave 1 utilizando WSAD
-        if (IsKeyDown('W')) {
-            posImagen1.y -= velocidad.y;
+        // Buffer de teclado que reinicia la posicion del personaje al presionar la R
+        if (IsKeyPressed(KEY_R)) {
+            posImagen1 = { 150, (768 / 2) };
         }
-        if (IsKeyDown('S')) {
-            posImagen1.y += velocidad.y;
+
+        // Buffer de teclado que muestra y oculta mensaje del sistema
+        if (IsKeyPressed(KEY_M)) {
+            contador++;
         }
-        if (IsKeyDown('A')) {
+
+        // Aplicamos movimiento horizontal  (izq y der ) sobre la Nave 1 utilizando flecha izquierda y derecha 
+        if (IsKeyDown(KEY_LEFT)) {
             posImagen1.x -= velocidad.x;
         }
-        if (IsKeyDown('D')) {
+        if (IsKeyDown(KEY_RIGHT)) {
             posImagen1.x += velocidad.x;
         }
 
-        // Aplicamos movimiento sobre la Nave 2 utilizando las flechas de direccion 
-        if (IsKeyDown(KEY_UP)) {
-            posImagen2.y -= velocidad.y;
-        }
-        if (IsKeyDown(KEY_DOWN)) {
-            posImagen2.y += velocidad.y;
-        }
-        if (IsKeyDown(KEY_LEFT)) {
-            posImagen2.x -= velocidad.x;
-        }
-        if (IsKeyDown(KEY_RIGHT)) {
-            posImagen2.x += velocidad.x;
-        }
+        // Captura de eventos del mouse
+            
+            // Devuelve la posicion del mouse
+        Vector2 windowPosition = GetMousePosition();
+        bool collision = CheckCollisionPointCircle(windowPosition, Boton, r);
 
-        // Aplicamos un SWAP que invierte las propiedades
-        if (IsKeyPressed(KEY_SPACE)) {
+        if (collision && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 
-            contador++;
-
-            if ((contador % 2) == 0) {
-                rotImagen1 = 180.0f;
-                colImagen1 = { 0,158,47,255 };
-                escImagen1 = 0.3f;
-
-                rotImagen2 = 0.0f;
-                colImagen2 = { 255,161,0,255 };
-                escImagen2 = 0.5f;
-            }
-            else
-            {
-                rotImagen1 = 0.0f;
-                colImagen1 = { 255,161,0,255 };
-                escImagen1 = 0.5f;
-
-                rotImagen2 = 180.0f;
-                colImagen2 = { 0,158,47,255 };
-                escImagen2 = 0.3f;
-            }
+            fondo.r = GetRandomValue(0, 255);
+            fondo.g = GetRandomValue(0, 255);
+            fondo.b = GetRandomValue(0, 255);
+            
         }
-           
      
         // Iniciamos la etapa de dibujo
         BeginDrawing();
 
         // Limpiamos la pantalla 
-        ClearBackground(RAYWHITE);
+        ClearBackground(fondo);
+        
+        // Mensaje de sistema
+        if (contador % 2 == 0) {
+            DrawText("Nave 1", 2, 0, 25, BLACK);
+            DrawText("Haz click izquierdo sobre el circulo negro para cambiar el color del fondo", 2, 25, 15, BLACK);
+            DrawText(TextFormat("Posicion: %.1f, %.1f", posImagen1.x, posImagen1.y), 2, 70, 15, BLACK);
+            DrawText(TextFormat("Posicion del mouse: X: %.2f Y: %.2f", windowPosition.x, windowPosition.y), 2, 50, 15, BLACK);
+        }
 
         // Dibujamos la imagen para la nave 1 con sus parametros personalizados
         DrawTextureEx(imagen, posImagen1, rotImagen1, escImagen1, colImagen1);
 
-        // Dibujamos la imagen para la nvae 2 con otros parametros personalizados
-        DrawTextureEx(imagen, posImagen2, rotImagen2, escImagen2, colImagen2);
+        // Dibujamos el boton para cambiar el color del fondo
+        DrawCircle(Boton.x, Boton.y, r, colorBoton);
 
-        // Texto con la informacion del sprite "nave 1"
-        DrawText("Nave 1", 2, 0, 25, BLACK);
-        DrawText(TextFormat("Escala: %.1f", escImagen1), 2, 20, 25, BLACK);
-        DrawText(TextFormat("Color(RGBA): R%d, G%d, B%d, A%d", colImagen1.r, colImagen1.g, colImagen1.b, colImagen1.a), 2, 50, 25, BLACK);
-        DrawText(TextFormat("Posicion: %.1f, %.1f", posImagen1.x, posImagen1.y), 2, 70, 25, BLACK);
+        // Dibujamos el cursor con la posicion del mouse
+        DrawTexturePro(cursor, cursorRectangle, {windowPosition.x, windowPosition.y, cursorRectangle.width, cursorRectangle.height}, cursorCenter, 0, WHITE);
 
-        // Texto con la informacion del sprite "nave 2"
-        DrawText("Nave 2", 500, 0, 25, BLACK);
-        DrawText(TextFormat("Escala: %.1f", escImagen2), 500, 20, 25, BLACK);
-        DrawText(TextFormat("Color(RGBA): R%d, G%d, B%d, A%d", colImagen2.r, colImagen2.g, colImagen2.b, colImagen2.a), 500, 50, 25, BLACK);
-        DrawText(TextFormat("Posicion: %.1f, %.1f", posImagen2.x, posImagen2.y), 500, 70, 25, BLACK);
 
-        
         // Finalizamos el dibujo
         EndDrawing();
     }
 
+    // Descargamos la imagen para liberar recursos
     UnloadTexture(imagen);
+
     // Cerramos la ventana
     CloseWindow();
 
